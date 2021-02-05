aad677d90af3f857e403c5b9f68859b040b5d46c
yesaya.yovand <49329034+yesayayova@users.noreply.github.com>
2021-02-05 16:37:54 +0700

    Update importTrainData.py

--- importTrainData.py
+++ importTrainData.py
@@ -1,5 +1,6 @@
 import os
 
+#importing training data from local
 def imlist(path):
     return [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.jpg')]
  
@@ -15,41 +16,50 @@ import numpy as np
 import cv2
 
 images = []
-
 for img in angry:
     x = cv2.imread(img)
     x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
     images.append(x)
-
 for img in disgust:
     x = cv2.imread(img)
     x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
     images.append(x)
-
 for img in fear:
     x = cv2.imread(img)
     x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
     images.append(x)
-
 for img in happy:
     x = cv2.imread(img)
-    x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
-    images.append(x)
-
 for img in neutral:
     x = cv2.imread(img)
     x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
     images.append(x)
-
 for img in sad:
     x = cv2.imread(img)
     x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
     images.append(x)
-
 for img in surprise:
     x = cv2.imread(img)
     x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
     images.append(x)
-
 x_train = np.array(images)
 x_train.shape
+
+#build label data train
+labels = []
+for i in range(len(angry)):
+    labels.append(0)
+for i in range(len(disgust)):
+    labels.append(1)
+for i in range(len(fear)):
+    labels.append(2)
+for i in range(len(happy)):
+    labels.append(3)
+for i in range(len(neutral)):
+    labels.append(4)
+for i in range(len(sad)):
+    labels.append(5)
+for i in range(len(surprise)):
+    labels.append(6)
+    x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
+    images.append(x)
